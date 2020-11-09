88f013f22cc9c328eeb8e0570542d7df30729bdc
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2020-11-09 09:41:16 +0300

    fixed crud save method

--- src/pages/CrudDemo.vue
+++ src/pages/CrudDemo.vue
@@ -178,7 +178,7 @@ export default {
 
 			if (this.product.name.trim()) {
 				if (this.product.id) {
-					this.$set(this.products, this.findIndexById(this.product.id), this.product);
+					this.products[this.findIndexById(this.product.id)] = this.product;
 					this.$toast.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
 				}
 				else {
