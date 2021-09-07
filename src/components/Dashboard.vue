d78c3714b322cd5ea2cfab94a1e905a2ee610853
Cagatay Civici <cagatay.civici@gmail.com>
2021-09-07 14:04:24 +0300

    Dashboard redone with primeflex 3

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,100 +1,114 @@
  <template>
 <div class="grid p-fluid dashboard">
 	<div class="col-12 lg:col-4">
-		<div class="card summary">
-			<span class="title">Users</span>
-			<span class="detail">Number of visitors</span>
-			<span class="count visitors">12</span>
+		<div class="card flex justify-content-between align-items-start mb-0">
+			<div>
+				<span class="text-2xl">Users</span>
+				<span class="p-text-secondary block mt-2">Number of visitors</span>
+			</div>
+			<span class="bg-green-500 text-white py-2 px-3 text-2xl border-round">12</span>
 		</div>
 	</div>
 	<div class="col-12 lg:col-4">
-		<div class="card summary">
-			<span class="title">Sales</span>
-			<span class="detail">Number of purchases</span>
-			<span class="count purchases">534</span>
+		<div class="card flex justify-content-between align-items-start mb-0">
+			<div>
+				<span class="text-2xl">Sales</span>
+				<span class="p-text-secondary block mt-2">Number of purchases</span>
+			</div>
+			<span class="bg-purple-500 text-white py-2 px-3 text-2xl border-round">534</span>
 		</div>
 	</div>
 	<div class="col-12 lg:col-4">
-		<div class="card summary">
-			<span class="title">Revenue</span>
-			<span class="detail">Income for today</span>
-			<span class="count revenue">$3,200</span>
+		<div class="card flex justify-content-between align-items-start mb-0">
+			<div>
+				<span class="text-2xl">Revenue</span>
+				<span class="p-text-secondary block mt-2">Income for today</span>
+			</div>
+			<span class="bg-blue-500 text-white py-2 px-3 text-2xl border-round">$320</span>
 		</div>
 	</div>
 
 	<div class="col-12 md:col-6 xl:col-3">
-		<div class="highlight-box">
-			<div class="initials" style="background-color: #007be5; color: #00448f"><span>TV</span></div>
-			<div class="highlight-details ">
-				<i class="pi pi-search"></i>
-				<span>Total Queries</span>
-				<span class="count">523</span>
+		<div class="flex">
+			<div class="flex-1 bg-indigo-500 text-white p-3 text-7xl flex align-items-center justify-content-center"><span>TQ</span></div>
+			<div class="flex-1 bg-white p-3">
+				<div class="flex flex align-items-center">
+					<i class="pi pi-search text-lg mr-2"></i>
+					<span>Total Queries</span>
+				</div>
+				<span class="block mt-2 p-text-secondary text-5xl">523</span>
 			</div>
 		</div>
 	</div>
 	<div class="col-12 md:col-6 xl:col-3">
-		<div class="highlight-box">
-			<div class="initials" style="background-color: #ef6262; color: #a83d3b"><span>TI</span></div>
-			<div class="highlight-details ">
-				<i class="pi pi-question-circle"></i>
-				<span>Total Issues</span>
-				<span class="count">81</span>
+		<div class="flex">
+			<div class="flex-1 bg-purple-500 text-white p-3 text-7xl flex align-items-center justify-content-center"><span>TI</span></div>
+			<div class="flex-1 bg-white p-3">
+				<div class="flex flex align-items-center">
+					<i class="pi pi-search text-lg mr-2"></i>
+					<span>Total Issues</span>
+				</div>
+				<span class="block mt-2 p-text-secondary text-5xl">81</span>
 			</div>
 		</div>
 	</div>
 	<div class="col-12 md:col-6 xl:col-3">
-		<div class="highlight-box">
-			<div class="initials" style="background-color: #20d077; color: #038d4a"><span>OI</span></div>
-			<div class="highlight-details ">
-				<i class="pi pi-filter"></i>
-				<span>Open Issues</span>
-				<span class="count">21</span>
+		<div class="flex">
+			<div class="flex-1 bg-orange-500 text-white p-3 text-7xl flex align-items-center justify-content-center"><span>OI</span></div>
+			<div class="flex-1 bg-white p-3">
+				<div class="flex flex align-items-center">
+					<i class="pi pi-search text-lg mr-2"></i>
+					<span>Open Issues</span>
+				</div>
+				<span class="block mt-2 p-text-secondary text-5xl">21</span>
 			</div>
 		</div>
 	</div>
 	<div class="col-12 md:col-6 xl:col-3">
-		<div class="highlight-box">
-			<div class="initials" style="background-color: #f9c851; color: #b58c2b"><span>CI</span></div>
-			<div class="highlight-details ">
-				<i class="pi pi-check"></i>
-				<span>Closed Issues</span>
-				<span class="count">60</span>
+		<div class="flex">
+			<div class="flex-1 bg-bluegray-500 text-white p-3 text-7xl flex align-items-center justify-content-center"><span>CI</span></div>
+			<div class="flex-1 bg-white p-3">
+				<div class="flex flex align-items-center">
+					<i class="pi pi-search text-lg mr-2"></i>
+					<span>Closed Issues</span>
+				</div>
+				<span class="block mt-2 p-text-secondary text-5xl">60</span>
 			</div>
 		</div>
 	</div>
 
 	<div class="col-12 md:col-6 lg:col-4">
-		<Panel header="Tasks" style="height: 100%">
-			<ul class='task-list'>
-				<li>
+		<Panel header="Tasks">
+			<ul class="list-none m-0 p-0">
+				<li class="py-2 px-1 border-bottom-1 surface-border flex align-items-center">
 					<Checkbox name="task" value="reports" v-model="tasksCheckbox" />
-					<span class="task-name">Sales Reports</span>
-					<i class="pi pi-chart-bar" />
+					<span class="ml-2">Sales Reports</span>
+					<i class="pi pi-chart-bar ml-auto" />
 				</li>
-				<li>
+				<li class="py-2 px-1 border-bottom-1 surface-border flex align-items-center">
 					<Checkbox name="task" value="invoices" v-model="tasksCheckbox" />
-					<span class="task-name">Pay Invoices</span>
-					<i class="pi pi-dollar" />
+					<span class="ml-2">Pay Invoices</span>
+					<i class="pi pi-dollar ml-auto" />
 				</li>
-				<li>
+				<li class="py-2 px-1 border-bottom-1 surface-border flex align-items-center">
 					<Checkbox name="task" value="dinner" v-model="tasksCheckbox" />
-					<span class="task-name">Dinner with Tony</span>
-					<i class="pi pi-user" />
+					<span class="ml-2">Dinner with Tony</span>
+					<i class="pi pi-user ml-auto" />
 				</li>
-				<li>
+				<li class="py-2 px-1 border-bottom-1 surface-border flex align-items-center">
 					<Checkbox name="task" value="meeting" v-model="tasksCheckbox" />
-					<span class="task-name">Client Meeting</span>
-					<i class="pi pi-users" />
+					<span class="ml-2">Client Meeting</span>
+					<i class="pi pi-users ml-auto" />
 				</li>
-				<li>
+				<li class="py-2 px-1 border-bottom-1 surface-border flex align-items-center">
 					<Checkbox name="task" value="theme" v-model="tasksCheckbox" />
-					<span class="task-name">New Theme</span>
-					<i class="pi pi-globe" />
+					<span class="ml-2">New Theme</span>
+					<i class="pi pi-globe ml-auto" />
 				</li>
-				<li>
+				<li class="py-2 px-1 flex align-items-center">
 					<Checkbox name="task" value="flight" v-model="tasksCheckbox" />
-					<span class="task-name">Flight Ticket</span>
-					<i class="pi pi-briefcase" />
+					<span class="ml-2">Flight Ticket</span>
+					<i class="pi pi-briefcase ml-auto" />
 				</li>
 			</ul>
 		</Panel>
@@ -124,34 +138,34 @@
 
 	<div class="col-12 lg:col-4 contacts">
 		<Panel header="Contacts">
-			<ul>
-				<li>
-					<button class="p-link">
-						<img src="assets/layout/images/avatar_1.png" width="35" alt="avatar1"/>
-						<span class="name">Claire Williams</span>
-						<span class="email">clare@primevue.com</span>
-					</button>
+			<ul class="list-none p-0 m-0">
+				<li class="py-2 border-bottom-1 surface-border flex align-items-center">
+					<img src="assets/layout/images/avatar_1.png" width="35" alt="avatar1"/>
+					<div class="ml-auto text-righ">
+						<span class="text-xl">Claire Williams</span>
+						<span class="p-text-secondary block">clare@primevue.com</span>
+					</div>
 				</li>
-				<li>
-					<button class="p-link">
-						<img src="assets/layout/images/avatar_2.png" width="35" alt="avatar2"/>
-						<span class="name">Jason Dourne</span>
-						<span class="email">jason@primevue.com</span>
-					</button>
+				<li class="py-2 border-bottom-1 surface-border flex align-items-center">
+					<img src="assets/layout/images/avatar_2.png" width="35" alt="avatar2"/>
+					<div class="ml-auto text-righ">
+						<span class="text-xl">Jason Dourne</span>
+						<span class="p-text-secondary block">jason@primevue.com</span>
+					</div>
 				</li>
-				<li>
-					<button class="p-link">
-						<img src="assets/layout/images/avatar_3.png" width="35" alt="avatar3"/>
-						<span class="name">Jane Davidson</span>
-						<span class="email">jane@primevue.com</span>
-					</button>
+				<li class="py-2 border-bottom-1 surface-border flex align-items-center">
+					<img src="assets/layout/images/avatar_3.png" width="35" alt="avatar3"/>
+					<div class="ml-auto text-righ">
+						<span class="text-xl">Jane Davidson</span>
+						<span class="ep-text-secondary block">jane@primevue.com</span>
+					</div>
 				</li>
-				<li>
-					<button class="p-link">
-						<img src="assets/layout/images/avatar_4.png" width="35" alt="avatar4"/>
-						<span class="name">Tony Corleone</span>
-						<span class="email">tony@primevue.com</span>
-					</button>
+				<li class="py-2 border-bottom-1 surface-border flex align-items-center">
+					<img src="assets/layout/images/avatar_4.png" width="35" alt="avatar4"/>
+					<div class="ml-auto text-right">
+						<span class="text-xl">Tony Corleone</span>
+						<span class="p-text-secondary block">tony@primevue.com</span>
+					</div>
 				</li>
 			</ul>
 		</Panel>
@@ -202,58 +216,54 @@
 
 	<div class="col-12 lg:col-4">
 		<Panel header="Activity" style="height: 100%">
-			<div class="activity-header">
-				<div class="grid">
-					<div class="col-6">
-						<span style="font-weight:bold">Last Activity</span>
-						<p>Updated 1 minute ago</p>
-					</div>
-					<div class="col-6" style="text-align: right">
-						<Button label="Refresh" icon="pi pi-refresh" />
-					</div>
+			<div class="flex align-items-start mb-3 pb-2 border-bottom-1 surface-border">
+				<div>
+					<span style="font-weight:bold">Last Activity</span>
+					<p>Updated 1 minute ago</p>
 				</div>
+				<Button label="Refresh" icon="pi pi-refresh" class="ml-auto w-auto" />
 			</div>
 
-			<ul class="activity-list">
-				<li>
+			<ul class="list-none p-0 m-0">
+				<li class="py-3 px-2 border-bottom-1 surface-border">
                     <div class="flex justify-content-between align-items-center mb-3">
-                        <h5 class="activity m-0">Income</h5>
-                        <div class="count">$900</div>
+                        <span class="text-2xl">Income</span>
+                        <div class="font-bold bg-cyan-500 text-white p-2 border-round text-2xl">$900</div>
                     </div>
                     <ProgressBar :value="95" :showValue="false" />
 				</li>
-				<li>
+				<li class="py-3 px-2 border-bottom-1 surface-border">
 					<div class="flex justify-content-between align-items-center mb-3">
-						<h5 class="activity m-0">Tax</h5>
-						<div class="count" style="background-color:#f9c851">$250</div>
+						<h5 class="text-2xl">Tax</h5>
+						<div class="font-bold bg-purple-500 text-white p-2 border-round text-2xl" style="background-color:#f9c851">$250</div>
 					</div>
 					<ProgressBar :value="24" :showValue="false" />
 				</li>
-				<li>
+				<li class="py-3 px-2 border-bottom-1 surface-border">
 					<div class="flex justify-content-between align-items-center mb-3">
-						<h5 class="activity m-0">Invoices</h5>
-						<div class="count" style="background-color:#20d077">$125</div>
+						<h5 class="text-2xl">Invoices</h5>
+						<div class="font-bold bg-indigo-500 text-white p-2 border-round text-2xl" style="background-color:#20d077">$125</div>
 					</div>
 					<ProgressBar :value="55" :showValue="false" />
 				</li>
-				<li>
+				<li class="py-3 px-2 border-bottom-1 surface-border">
 					<div class="flex justify-content-between align-items-center mb-3">
-						<h5 class="activity m-0">Expenses</h5>
-						<div class="count" style="background-color:#f9c851">$250</div>
+						<h5 class="text-2xl">Expenses</h5>
+						<div class="font-bold bg-orange-500 text-white p-2 border-round text-2xl" style="background-color:#f9c851">$250</div>
 					</div>
 					<ProgressBar :value="15" :showValue="false" />
 				</li>
-				<li>
+				<li class="py-3 px-2 border-bottom-1 surface-border">
 					<div class="flex justify-content-between align-items-center mb-3">
-						<h5 class="activity m-0">Bonus</h5>
-						<div class="count" style="background-color:#007be5">$350</div>
+						<h5 class="text-2xl">Bonus</h5>
+						<div class="font-bold bg-red-500 text-white p-2 border-round text-2xl" style="background-color:#007be5">$350</div>
 					</div>
 					<ProgressBar :value="5" :showValue="false" />
 				</li>
-				<li>
+				<li class="py-3 px-2">
 					<div class="flex justify-content-between align-items-center mb-3">
-						<h5 class="activity m-0">Revenue</h5>
-						<div class="count" style="background-color:#ef6262">$500</div>
+						<h5 class="text-2xl">Revenue</h5>
+						<div class="font-bold bg-green-500 text-white p-2 border-round text-2xl" style="background-color:#ef6262">$500</div>
 					</div>
 					<ProgressBar :value="25" :showValue="false" />
 				</li>
