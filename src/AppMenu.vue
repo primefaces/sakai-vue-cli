c10c4743b7f906bb68a146551c8f014252403b9f
Cagatay Civici <cagatay.civici@gmail.com>
2021-09-10 19:19:30 +0300

    Add primeblocks

--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="layout-menu-container">
 		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" />
+		<a href="https://www.primefaces.org/primeblocks-vue" class="block mt-3">
+			<img alt="primeblocks" :src="darkTheme ? 'images/banner-primeblocks.png' : 'images/banner-primeblocks-dark.png'" class="w-full" />
+		</a>
 	</div>
 </template>
 
@@ -16,6 +19,11 @@ export default {
             this.$emit('menuitem-click', event);
         }
     },
+	computed: {
+		darkTheme() {
+			return this.$appState.theme.startsWith('saga');
+		}
+	},
 	components: {
 		'AppSubmenu': AppSubmenu
 	}
