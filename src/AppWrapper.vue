f93ee2eabb3f3b728e7d665691041b984f04b6a0
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-01-19 16:41:44 +0300

    Revert "update changing theme"
    This reverts commit 03f66c19083eb13ff2328f077327b290197c5916.

--- src/AppWrapper.vue
+++ src/AppWrapper.vue
@@ -1,13 +1,14 @@
 <template>
-    <Landing v-if="$route.path === '/landing'" />
-    <Login v-else-if="$route.path === '/login'" />
+    <Landing v-if="$route.path === '/landing'" @change-theme="changeTheme" />
+    <Login v-else-if="$route.path === '/login'" @change-theme="changeTheme" />
     <Error v-else-if="$route.path === '/error'" />
     <NotFound v-else-if="$route.path === '/notfound'" />
     <Access v-else-if="$route.path === '/access'" />
-    <App v-else />
+    <App v-else @change-theme="changeTheme" />
 </template>
 
 <script>
+import EventBus from './AppEventBus';
 import App from './App';
 import Landing from './pages/LandingDemo';
 import Login from './pages/Login';
@@ -16,6 +17,19 @@ import NotFound from './pages/NotFound';
 import Access from './pages/Access';
 
 export default {
+    methods: {
+        changeTheme(event) {
+            let themeElement = document.getElementById('theme-link');
+            themeElement.setAttribute('href', themeElement.getAttribute('href').replace(this.$appState.theme, event.theme));
+            this.$appState.theme = event.theme;
+            this.$appState.darkTheme = event.dark;
+            EventBus.emit('change-theme', event);
+
+            if (event.theme.startsWith('md')) {
+                this.$primevue.config.ripple = true;
+            }
+        }
+    },
     components: {
         App,
         Landing,
