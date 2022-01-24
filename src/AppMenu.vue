f66aa7834a2f04a2e2ea18d9be7b7d3fecb37376
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-01-24 12:52:42 +0300

    menu accessibility added

--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="layout-menu-container">
-		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" />
+		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" @keydown="onKeyDown" />
 		<a href="https://www.primefaces.org/primeblocks-vue" class="block mt-3">
 			<img alt="primeblocks" :src="bannerImage()" class="w-full" />
 		</a>
@@ -18,6 +18,13 @@ export default {
         onMenuItemClick(event) {
             this.$emit('menuitem-click', event);
         },
+		onKeyDown(event) {
+			const nodeElement = event.target;
+			if (event.code === 'Enter' || event.code === 'Space') {
+				nodeElement.click();
+				event.preventDefault();
+			}
+		},
 		bannerImage() {
 			return this.$appState.darkTheme ? 'images/banner-primeblocks-dark.png' : 'images/banner-primeblocks.png';
 		}
--- src/AppSubmenu.vue
+++ src/AppSubmenu.vue
@@ -3,17 +3,17 @@
 		<template v-for="(item,i) of items">
 			<li v-if="visible(item) && !item.separator" :key="item.label || i" :class="[{'layout-menuitem-category': root, 'active-menuitem': activeIndex === i && !item.to && !item.disabled}]" role="none">
 				<template v-if="root">
-					<div class="layout-menuitem-root-text">{{item.label}}</div>
+					<div class="layout-menuitem-root-text" :aria-label="item.label">{{item.label}}</div>
 					<appsubmenu :items="visible(item) && item.items" @menuitem-click="$emit('menuitem-click', $event)"></appsubmenu>
 				</template>
 				<template v-else>
-					<router-link v-if="item.to" :to="item.to" :class="[item.class, 'p-ripple', {'p-disabled': item.disabled}]" :style="item.style" @click="onMenuItemClick($event,item,i)" :target="item.target" exact role="menuitem" v-ripple>
+					<router-link v-if="item.to" :to="item.to" :class="[item.class, 'p-ripple', {'p-disabled': item.disabled}]" :style="item.style" @click="onMenuItemClick($event,item,i)" :target="item.target" :aria-label="item.label" exact role="menuitem" v-ripple>
 						<i :class="item.icon"></i>
 						<span>{{item.label}}</span>
 						<i v-if="item.items" class="pi pi-fw pi-angle-down menuitem-toggle-icon"></i>
 						<Badge v-if="item.badge" :value="item.badge"></Badge>
 					</router-link>
-					<a v-if="!item.to" :href="item.url||'#'" :style="item.style" :class="[item.class, 'p-ripple', {'p-disabled': item.disabled}]" @click="onMenuItemClick($event,item,i)" :target="item.target" role="menuitem" v-ripple>
+					<a v-if="!item.to" :href="item.url||'#'" :style="item.style" :class="[item.class, 'p-ripple', {'p-disabled': item.disabled}]" @click="onMenuItemClick($event,item,i)" :target="item.target" :aria-label="item.label" role="menuitem" v-ripple>
 						<i :class="item.icon"></i>
 						<span>{{item.label}}</span>
 						<i v-if="item.items" class="pi pi-fw pi-angle-down menuitem-toggle-icon"></i>
