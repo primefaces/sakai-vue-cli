f73cf321d8d23cab36e75c12e7fa19b420e2e0a8
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-01-25 13:55:22 +0300

    update background

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="surface-50 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+    <div class="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
         <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
             <div class="col-12 mt-5 xl:mt-0 text-center">
                 <img :src="'layout/images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5" style="width:81px; height:60px;">
             </div>
             <div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
-                <div class="surface-section h-full w-full m-0 py-7 px-4" style="border-radius:53px;">
+                <div class="h-full w-full m-0 py-7 px-4" style="border-radius:53px; background: linear-gradient(180deg, var(--surface-0) 38.9%, var(--surface-0));">
                     <div class="text-center mb-5">
                         <img src="layout/images/avatar.png" alt="Image" height="50" class="mb-3">
                         <div class="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
