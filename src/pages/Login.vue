259cf396e948980eb132a02430a26e57b2e8c3af
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-01-25 11:11:50 +0300

    placeholder added

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -14,7 +14,7 @@
                 
                     <div class="w-full md:w-10 mx-auto">
                         <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" v-model="email" type="text" class="w-full mb-3" style="padding:1rem;" />
+                        <InputText id="email1" v-model="email" type="text" class="w-full mb-3" placeholder="Email" style="padding:1rem;" />
                 
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
                         <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem"></Password>
